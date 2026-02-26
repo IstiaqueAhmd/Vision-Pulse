54f48ddc867c26f9b390d9168008e11678cd7aa1
Istiaque Ahmed <141866100+IstiaqueAhmd@users.noreply.github.com>
2026-02-26 16:02:39 +0600

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -9,7 +9,8 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=True) # Nullable because of OAuth users
+    auth_provider = Column(String, default="local") # "local", "google", etc.
     reset_otp = Column(String, nullable=True)
     otp_expires_at = Column(DateTime, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
