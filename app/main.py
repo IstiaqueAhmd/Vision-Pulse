645212abe6ef746879a444ec0d7bb4bcdf99c7de
Istiaque Ahmed <141866100+IstiaqueAhmd@users.noreply.github.com>
2026-02-26 12:45:25 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.api.v1.api import api_router
-from app.core.config import settings # Note: config might be empty depending on structure
+from app.core.config import settings
+
+from app.db.session import engine
+from app.db.base import Base
+from app.models.user import User
+from app.models.video import Video
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Vision-Pulse API")
 
