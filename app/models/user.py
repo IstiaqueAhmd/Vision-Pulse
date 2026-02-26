2916e60f2d292f3c6f2ca04c1cdcb0edf7bc2acc
Istiaque Ahmed <141866100+IstiaqueAhmd@users.noreply.github.com>
2026-02-26 11:43:59 +0600

    Create user.py

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    reset_otp = Column(String, nullable=True)
+    otp_expires_at = Column(DateTime, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
