9d4ca63689710b871b5394413cfdce9c6857e830
Istiaque Ahmed <141866100+IstiaqueAhmd@users.noreply.github.com>
2026-03-07 14:52:21 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ app = FastAPI(
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","http://localhost:3001"],  # Allows all origins
+    allow_origins=["http://localhost:3000","http://localhost:3001","http://10.10.12.27:3000"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
