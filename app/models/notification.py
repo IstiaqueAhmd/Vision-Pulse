6d9d5351b54fc7dc910bc499c24cefd087581bc9
IstiaqueAhmd <istiaque@joinventureai.com>
2026-04-24 15:48:22 +0600

    add notification status table

--- app/models/notification.py
+++ app/models/notification.py
@@ -18,3 +18,17 @@ class Notification(Base):
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     user = relationship("User")
+
+class NotificationSettings(Base):
+    __tablename__ = "notification_settings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
+    email = Column(Boolean, default=True, nullable=False)
+    low_balance = Column(Boolean, default=True, nullable=False)
+    payment = Column(Boolean, default=True, nullable=False)
+    video_status = Column(Boolean, default=True, nullable=False)
+    message = Column(Boolean, default=True, nullable=False)
+    product_update = Column(Boolean, default=True, nullable=False)
+
+    user = relationship("User")
